e5307c1959481ab959881e2250adc39fe1e26c03
karenthiran <kkarenthiran@gmail.com>
2025-04-09 02:15:42 +0530

    Created a file for design the sign-in and sign-up page

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout=({children}:{children:React.ReactNode})=>{
+    return (
+        <div className="h-screen flex item-center justify-center"> 
+            {children}
+        </div>
+    );
+}
+
+export default ClerkLayout
