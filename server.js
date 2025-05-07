c5778e70fb0d0d26ff767d327fdc8bebd22bd080
karenthiran <kkarenthiran@gmail.com>
2025-05-07 11:51:38 +0530

    updated server js file

--- server.js
+++ server.js
@@ -1,46 +1,126 @@
-import next from "next";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
+const path = require('path');
+const express = require('express');
+const app = express();
+const socketIO = require('socket.io');
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
-const handler = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development';
 
-app.prepare().then(() => {
-  const httpServer = createServer(handler);
+// Redirect to https
+app.get('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https' && env !== 'development') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+});
 
-  const io = new Server(httpServer);
-  let onlineUsers=[]
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-  io.on("connection", (socket) => {
-    // add user
-    socket.on('addNewUser',(clerkUser)=>{
-      clerkUser && !onlineUsers.some(user=>user?.userId === clerkUser.id) && on.onlineUsers.push({
-        userId: clerkUser.id,
-        socketId: socket.id,
-        profile: clerkUser
-      })
+const server = require('http').createServer(app);
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
 
-      io.emit('getUsers',onlineUsers)
-    })
+/**
+ * Socket.io events
+ */
+const io = socketIO(server);
 
-    socket.on('disconnect',()=>{
-      onlineUsers = onlineUsers.filter(user => user.socketId != socket.id)
 
-      //send online users
-      io.emit('getUsers',onlineUsers)
-    })
-  });
+io.sockets.on('connection', function (socket) {
+    socket.on('chat message', ({ message, room }) => {
+    socket.broadcast.to(room).emit('chat message', { sender: socket.id, message });
+});
+    /**
+     * Log actions to the client
+     */
+    function log() {
+        const array = ['Server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+    }
 
-  httpServer
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+    /**
+     * Handle message from a client
+     * If toId is provided message will be sent ONLY to the client with that id
+     * If toId is NOT provided and room IS provided message will be broadcast to that room
+     * If NONE is provided message will be sent to all clients
+     */
+    socket.on('message', (message, toId = null, room = null) => {
+        log('Client ' + socket.id + ' said: ', message);
+
+        if (toId) {
+            console.log('From ', socket.id, ' to ', toId, message.type);
+
+            io.to(toId).emit('message', message, socket.id);
+        } else if (room) {
+            console.log('From ', socket.id, ' to room: ', room, message.type);
+
+            socket.broadcast.to(room).emit('message', message, socket.id);
+        } else {
+            console.log('From ', socket.id, ' to everyone ', message.type);
+
+            socket.broadcast.emit('message', message, socket.id);
+        }
+    });
+
+    let roomAdmin; // save admins socket id (will get overwritten if new room gets created)
+
+    /**
+     * When room gets created or someone joins it
+     */
+    socket.on('create or join', (room) => {
+        log('Create or Join room: ' + room);
+
+        // Get number of clients in the room
+        const clientsInRoom = io.sockets.adapter.rooms.get(room);
+        let numClients = clientsInRoom ? clientsInRoom.size : 0;
+
+        if (numClients === 0) {
+            // Create room
+            socket.join(room);
+            roomAdmin = socket.id;
+            socket.emit('created', room, socket.id);
+        } else {
+            log('Client ' + socket.id + ' joined room ' + room);
+
+            // Join room
+            io.sockets.in(room).emit('join', room); // Notify users in room
+            socket.join(room);
+            io.to(socket.id).emit('joined', room, socket.id); // Notify client that they joined a room
+            io.sockets.in(room).emit('ready', socket.id); // Room is ready for creating connections
+        }
+    });
+
+    /**
+     * Kick participant from a call
+     */
+    socket.on('kickout', (socketId, room) => {
+        if (socket.id === roomAdmin) {
+            socket.broadcast.emit('kickout', socketId);
+            io.sockets.sockets.get(socketId).leave(room);
+        } else {
+            console.log('not an admin');
+        }
+    });
+
+    // participant leaves room
+    socket.on('leave room', (room) => {
+        socket.leave(room);
+        socket.emit('left room', room);
+        socket.broadcast.to(room).emit('message', { type: 'leave' }, socket.id);
+    });
+
+    /**
+     * When participant leaves notify other participants
+     */
+    socket.on('disconnecting', () => {
+        socket.rooms.forEach((room) => {
+            if (room === socket.id) return;
+            socket.broadcast
+                .to(room)
+                .emit('message', { type: 'leave' }, socket.id);
+        });
     });
 });
